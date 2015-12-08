c26b50989560461a29bd437903f9ad2ceef19b68
Petr Messner <petr.messner@gmail.com>
2015-12-08 15:02:38 +0100

    script renamed: instant_mongo -> instant-mongo

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'instant_mongo=instant_mongo:main',
+            'instant-mongo=instant_mongo:main',
         ],
     },
 )
