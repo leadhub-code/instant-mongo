6794da450871a5514fe4e0ab9ed464c7bc25d242
Petr Messner <petr.messner@gmail.com>
2015-12-08 15:21:26 +0100

    script: --bind-ip is alias to --bind_ip

--- instant_mongo/cli.py
+++ instant_mongo/cli.py
@@ -8,7 +8,7 @@ def main():
     p = argparse.ArgumentParser()
     p.add_argument('--dbpath', default='instant-mongo-data')
     p.add_argument('--port', default=27017)
-    p.add_argument('--bind_ip', default='127.0.0.1')
+    p.add_argument('--bind_ip', '--bind-ip', default='127.0.0.1')
     args = p.parse_args()
     im = InstantMongoDB(data_dir=args.dbpath, port=args.port, bind_ip=args.bind_ip,
         wired_tiger=True, wired_tiger_zlib=True)
