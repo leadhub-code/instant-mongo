863866fd73bf52d9362f0ae90fa2f11ec1cd1316
Petr Messner <petr.messner@gmail.com>
2015-12-08 15:24:11 +0100

    fixed parsing --port as int

--- instant_mongo/cli.py
+++ instant_mongo/cli.py
@@ -7,7 +7,7 @@ from .instant_mongodb import InstantMongoDB
 def main():
     p = argparse.ArgumentParser()
     p.add_argument('--dbpath', default='instant-mongo-data')
-    p.add_argument('--port', default=27017)
+    p.add_argument('--port', type=int, default=27017)
     p.add_argument('--bind_ip', '--bind-ip', default='127.0.0.1')
     args = p.parse_args()
     im = InstantMongoDB(data_dir=args.dbpath, port=args.port, bind_ip=args.bind_ip,
