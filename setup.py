964cf3a5125452fc470607a66e6e96d6173e417f
Petr Messner <petr.messner@gmail.com>
2016-01-05 18:23:27 +0100

    version 0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ readme = (here / 'README.md').open(encoding='UTF-8').read()
 
 setup(
     name='instant-mongo',
-    version='0.1.1',
+    version='0.1.2',
     description='Launch Mongodb instance for dev and testing',
     long_description=readme,
     url='https://github.com/messa/instant-mongo',
