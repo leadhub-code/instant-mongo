066243f000c815f7f0e7d81d2dd2f03d3cd6d1e4
Petr Messner <petr.messner@gmail.com>
2016-01-21 12:27:28 +0100

    CLI signal handling

--- instant_mongo/cli.py
+++ instant_mongo/cli.py
@@ -15,8 +15,22 @@ def main():
     logging.basicConfig(
         level=logging.DEBUG if args.verbose else logging.INFO,
         format='%(levelname)5s: %(message)s')
+    setup_signals()
     im = InstantMongoDB(data_dir=args.dbpath, port=args.port, bind_ip=args.bind_ip,
         wired_tiger=True, wired_tiger_zlib=True)
     with im:
         while True:
             sleep(5)
+
+
+_exit_fired = False
+
+def setup_signals():
+    import signal
+    def h(signum, frame):
+        global _exit_fired
+        if not _exit_fired:
+            _exit_fired = True
+            raise SystemExit()
+    signal.signal(signal.SIGTERM, h)
+    signal.signal(signal.SIGINT, h)
